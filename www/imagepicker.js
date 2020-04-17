8e52a7ae149d38cb54a7d70b9c274e139d275379
Stefano <13023029+Missile71@users.noreply.github.com>
2020-04-17 12:09:53 +0200

    Update imagepicker.js

--- www/imagepicker.js
+++ www/imagepicker.js
@@ -62,7 +62,7 @@ ImagePicker.prototype.getPictures = function(success, fail, options) {
 		title: options.title ? options.title : 'Select an Album', // the default is the message of the old plugin impl
 		message: options.message ? options.message : null, // the old plugin impl didn't have it, so passing null by default
 		outputType: options.outputType ? options.outputType : this.OutputType.FILE_URI,
-		disable_popover: options.disable_popover ? options.disable_popover : false // Disable the iOS popover as seen on iPad
+		disable_popover: options.disable_popover ? options.disable_popover : true // Disable the iOS popover as seen on iPad
 	};
 
 	return cordova.exec(success, fail, "ImagePicker", "getPictures", [params]);
